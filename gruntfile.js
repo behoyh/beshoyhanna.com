ec9b90ed62c92ca3378ea3610344a2ef08310fb8
root <beshoyhanna@outlook.com>
2017-08-05 14:52:11 -0400

    changes

--- gruntfile.js
+++ gruntfile.js
@@ -288,4 +288,5 @@ module.exports = function (grunt) {
 
   // Run the project in production mode
   grunt.registerTask('prod', ['build', 'env:prod', 'mkdir:upload', 'copy:localConfig', 'concurrent:default']);
+  grunt.loadNpmTasks('grunt-forever');
 };
