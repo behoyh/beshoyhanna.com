6c7fe83ce8add846610c758139205d99d0365e75
beshoy hanna <beshoyhanna@Beshoys-MacBook.local>
2018-02-24 11:55:49 -0500

    Remove bg function

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -9,12 +9,6 @@
     var vm = this;
 
     var randomColor = 'rgba(' + (Math.floor(Math.random() * 256)) + ', '+ (Math.floor(Math.random() * 256)) + ', '+ (Math.floor(Math.random() * 256)) + ', '+ Math.random() + ')';
-    if (jQuery('#zen-intro') !== null) {
-      jQuery('#zen-intro').css("backgroundColor", randomColor);
-      setInterval(function() {
-        randomColor = 'rgba(' + (Math.floor(Math.random() * 256)) + ', '+ (Math.floor(Math.random() * 256)) + ', '+ (Math.floor(Math.random() * 256)) + ', '+ Math.random() + ')';
-        jQuery('#zen-intro').animate({backgroundColor: randomColor },7500);
-      }, 8000);
-    }
+
   }
 }())
